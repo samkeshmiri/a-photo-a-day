feeb964ea668e3e700b842f58e539781650a7f8f
Sam Keshmiri <samkeshmiri94@gmail.com>
2026-05-17 21:38:44 +0100

    chore: update compileSdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.skeshmiri.aphotoaday"
-    compileSdkPreview = "CinnamonBun"
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.skeshmiri.aphotoaday"
